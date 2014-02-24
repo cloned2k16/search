daef14dc60dd3ff775437bf03bae14035214fed6
Fahad Ibnay Heylaal <fahad19@gmail.com>
2014-02-25 00:28:33 +0100

    latest angular1.2.14-* requires html5Mode to be set manually.
    other query params keep getting appended multiple times as duplicates.

--- js/config/bootstrap.js
+++ js/config/bootstrap.js
@@ -4,6 +4,13 @@
 
 	var App = angular.module('BowerComponents', []);
 
+	App.config([
+		'$locationProvider',
+		function ($locationProvider) {
+			$locationProvider.html5Mode(true);
+		}
+	]);
+
 	require('../directives/shortcut')(App);
 	require('../filters/format_date')(App);
 	require('../controllers/index_controller')(App);
